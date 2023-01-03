08cbc2646fbadc72f56c0415e306db616ad50cad
Katrina <katrinalea.woolley@yahoo.co.uk>
2023-01-03 10:40:02 +0000

    format

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,9 @@ import cors from "cors";
 import dotenv from "dotenv";
 import { Client } from "pg"; //need to install
 
-
 // read in contents of key value pairs in the env file store as aenvironment variables (process.env)
 dotenv.config();
-const client = new Client({connectionString: process.env.DATABASE_URL});
+const client = new Client({ connectionString: process.env.DATABASE_URL });
 
 client.connect();
 const app = express();
@@ -18,35 +17,34 @@ app.use(cors());
 // use the environment variable PORT, or 4000 as a fallback
 const PORT_NUMBER = process.env.PORT ?? 4000;
 
-
 app.get("/", (req, res) => {
   res.status(200).json({
     status: "success",
-    data: "hello"
-  })
-})
+    data: "hello",
+  });
+});
 // GET /items
 app.get("/items", async (req, res) => {
   const text = "select * from toDo where completed = 'false'";
   const dbResponse = await client.query(text);
-    res.status(200).json({
-      status: "success",
-      data: dbResponse.rows
-    })
-  })
-   
+  res.status(200).json({
+    status: "success",
+    data: dbResponse.rows,
+  });
+});
 
 // POST /items
 app.post("/items", async (req, res) => {
   const { message, completed } = req.body;
-  console.log("whole req.bdoy", req.body)
+  console.log("whole req.bdoy", req.body);
   if (typeof message === "string") {
-    const text = "insert into toDo (message, completed) values ($1, $2) returning *";
+    const text =
+      "insert into toDo (message, completed) values ($1, $2) returning *";
     const values = [message, completed];
     const dbResult = await client.query(text, values);
     res.status(201).json({
       status: "success",
-      data: dbResult.rows
+      data: dbResult.rows,
     });
   } else {
     res.status(400).json({
@@ -62,54 +60,51 @@ app.get("/completed", async (req, res) => {
   const text = "select * from toDo where completed = 'true'";
   const dbResponse = await client.query(text);
   res.status(200).json({
-      status: "success",
-      data: dbResponse.rows
-    })
-  })
+    status: "success",
+    data: dbResponse.rows,
+  });
+});
 
 app.patch("/items/:id", async (req, res) => {
-  const {id} = req.body
-  const text = "update todo set completed = 'true' where id = $1"
-  const values = [id]
-  const dbResponse = await client.query(text, values)
+  const { id } = req.body;
+  const text = "update todo set completed = 'true' where id = $1";
+  const values = [id];
+  const dbResponse = await client.query(text, values);
   res.status(200).json({
     status: "success",
-    data: dbResponse.rows
-  })
-})
+    data: dbResponse.rows,
+  });
+});
 
 app.patch("/items/update/:id", async (req, res) => {
-  const {id, message} = req.body
-  const text = "update todo set message = $2 where id = $1"
-  const values = [id, message]
-  const dbResponse = await client.query(text, values)
+  const { id, message } = req.body;
+  const text = "update todo set message = $2 where id = $1";
+  const values = [id, message];
+  const dbResponse = await client.query(text, values);
   res.status(200).json({
     status: "success",
-    data: dbResponse.rows
-  })
-})
-
-
+    data: dbResponse.rows,
+  });
+});
 
 // DELETE /items/:id
 app.delete("/items/:id", async (req, res) => {
-  const id = req.body.id
+  const id = req.body.id;
   const text = "delete from toDo where id = $1";
-  const value = [id]
+  const value = [id];
   await client.query(text, value);
-    res.status(200).json({
-      status: "success"
-    })
-  })
+  res.status(200).json({
+    status: "success",
+  });
+});
 
 app.delete("/completed", async (req, res) => {
   const text = "delete from toDo where completed = 'true'";
   await client.query(text);
-    res.status(200).json({
-      status: "success",
-    })
-  })
-  
+  res.status(200).json({
+    status: "success",
+  });
+});
 
 app.listen(PORT_NUMBER, () => {
   console.log(`Server is listening on port ${PORT_NUMBER}!`);
